2eaa00bd1a5aadb134f8d04a132f732b00ebe321
Anthony PERARD <anthony.perard@citrix.com>
2018-08-16 15:46:06 +0100

    lib: Generate a tarball Xen from a git tree
    Handle the creation of a tarball from a Xen commit id. They are
    verification in place to check that the commit contain the tag of the
    release we want to build.
    
    We make use of `make src-tarball` without running ./configure first so
    its run is faked by `touch config/Tools.mk`. Still, that not enough, the
    system that will run make src-tarball will needs at least gcc.

--- lib/git.sh
+++ lib/git.sh
@@ -120,3 +120,29 @@ function git-get-branch
 
     report-result "$_branch"
 }
+
+# return 1 if git is older than the version specified by git_ver
+function git-check-version() {
+    $arg_parse
+    $requireargs git_ver
+
+    local v=$(git --version)
+    v=${v#git version }
+
+    # Compare digit of a version one-by-one
+    while [ -n "$git_ver" ]; do
+        [ -z "$v" ] && return 1
+        [ ${v%%.*} -lt ${git_ver%%.*} ] && return 1
+        [ ${v%%.*} -gt ${git_ver%%.*} ] && return 0
+        if [ "$v" != "${v#*.}" ]; then
+            v=${v#*.}
+        else
+            v=""
+        fi
+        if [ "$git_ver" != "${git_ver#*.}" ]; then
+            git_ver=${git_ver#*.}
+        else
+            git_ver=""
+        fi
+    done
+}
